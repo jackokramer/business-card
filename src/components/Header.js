f6e345e30b4b49f318b7ead21f032317965107ed
jackokramer <jackokramer@gmail.com>
2021-12-07 22:08:09 -0800

    added necessary text rather than filler

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header(){
         <div className="header">
             <img src={headshot} alt="placeholder" className="photo"/>
                 <h1 className="name">Jack Kramer</h1>
-                    <h4>Frontend Developer</h4>
+                    <h4>Fullstack Developer</h4>
                     <h5> <a href="jackokramer17.com" className="portfolio">jackkramer-portfolio</a></h5>
                     <a href="mailto:jackokramer17@gmail.com?subject=Mail from Our Site" className="Email"><svg viewBox="0 0 512 512" width="20" fill="#ffffff" title="envelope" className="envelope"/>Email</a>
                     <a href="https://www.linkedin.com/in/jack-kramer17/" className="LinkedIn"> LinkedIn</a>
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,11 @@ export default function Main(){
     return(
         <div className="about">
                 <h2 className="about-me-title">About Me</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora a veniam consectetur incidunt sunt fugit deleniti itaque! Corrupti eaque iure dicta quis nesciunt eum aspernatur excepturi sequi reiciendis voluptatum quaerat fugiat modi, in culpa reprehenderit sunt fugit ipsum sint ex neque dolorem quas? Quos nulla quis quia! Commodi, delectus perspiciatis!</p>
+                    <p>A certified fullstack devloper in Python(Django) and Ruby On Rails with over 7 years of Frontend devlopment work. Has severd as a conduit for large coporations as a conduit between deparments and teams. Fast, efficient and meticlous down to the minor details.</p>
             <h2 className="about-me-title">Interests Hobbies</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum eos commodi beatae consequuntur, ipsam blanditiis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum eos commodi beatae consequuntur, ipsam blanditiis.</p>
+                <p> Playing music, building guitar  pedals from scratch, reading and cooking</p>
+            <h2 className="about-me-title">Expertise</h2>
+                <p> Expert surfer and skateboarder with over 23 years of experince. I spent most of 2018 traveling globally looking for world class waves.</p>
             </div>
     )
 }
