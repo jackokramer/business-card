ff4dc4a3030a8c2627be2f9d7c2fe46d2976ddf5
Jack Kramer <31866908+jackokramer@users.noreply.github.com>
2022-02-15 15:19:21 -0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,8 @@ export default function Main(){
                     <p>A certified fullstack devloper in Python(Django) and Ruby On Rails with over 7 years of Frontend devlopment work. Has severd as a conduit for large coporations as a conduit between deparments and teams. Fast, efficient and meticlous down to the minor details.</p>
             <h2 className="about-me-title">Interests Hobbies</h2>
                 <p> Playing music, building guitar  pedals from scratch, reading and cooking</p>
-            <h2 className="about-me-title">Expertise</h2>
-                <p> Expert surfer and skateboarder with over 23 years of experince. I spent most of 2018 traveling globally looking for world class waves.</p>
+            <h2 className="about-me-title">Fun Fact</h2>
+                <p> I have been surfing and skateboarding for over 23 years. In 2018 spent most of it traveling globally looking for world class waves.</p>
             </div>
     )
 }
