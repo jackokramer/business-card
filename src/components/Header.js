5d7122edccf39c638be85c84b48c44a55a7c8159
Jack Kramer <31866908+jackokramer@users.noreply.github.com>
2022-02-14 17:17:20 -0800

    Update Header.js
    added my revamped portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header(){
             <img src={headshot} alt="placeholder" className="photo"/>
                 <h1 className="name">Jack Kramer</h1>
                     <h4>Fullstack Developer</h4>
-                    <h5> <a href="jackokramer17.com" className="portfolio">jackkramer-portfolio</a></h5>
+                    <h5> <a href="https://jacko-kramer-portfolio-revamp-sgijq.ondigitalocean.app/index.html" className="portfolio">jackkramer-portfolio</a></h5>
                     <a href="mailto:jackokramer17@gmail.com?subject=Mail from Our Site" className="Email"><svg viewBox="0 0 512 512" width="20" fill="#ffffff" title="envelope" className="envelope"/>Email</a>
                     <a href="https://www.linkedin.com/in/jack-kramer17/" className="LinkedIn"> LinkedIn</a>
             </div>
